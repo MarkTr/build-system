f9a83be5b23f96355b7c3e4e8ae58b9f3820af49
Mark Trompell <mark@foresightlinux.org>
2009-07-31 14:56:33 +0200

    add recipes as unfinished examples

--- refresh-xfce.py
+++ refresh-xfce.py
@@ -14,56 +14,127 @@ from optparse import OptionParser
 import subprocess as sp
 import os
 import conarypk
+from conary import cvc
+from rmake.cmdline import main as rmk
 
-versionString="%prog 0.1"	
+versionString="%prog 0.1"   
 gitPack = ['midori', 'ristretto']
 cwd = os.getcwd()
 cpk = conarypk.ConaryPk()
-ilp = 'foresight.rpath.org@fl:2-devel'
+ilps = {'xd':'xfce.rpath.org@xfce:devel','fl2':'foresight.rpath.org@fl:2-devel'}
+codirs = {'xd':'~/conary/src/xfce-devel/','fl2':'~/conary/src/fl2-devel/'}
 
-def latest_git(project, gitrepo):
-	curgit = cpk.request_query(project, ilp)[0][1].trailingRevision().getVersion()
-	#curgit.getSourceVersion()
-	print curgit
-	os.chdir(os.path.expanduser('~/git/'))
-	sp.check_call(['git-clone', '-q', '--depth', '1', '-n', gitrepo, project])
-	os.chdir(os.path.expanduser('~/git/') + project)		
-	newgit = 	sp.Popen(['git', 'rev-parse', '--short', 'HEAD'],
-	        stdout=sp.PIPE, stderr=sp.STDOUT).stdout.read().strip()
-	print newgit
-	os.chdir(cwd)
-	
-	
-def update(scm):
-	latest_git(project, gitrepo)
-	print (scm)
-	
+pkglist = [
+    'gtk-xfce-engine',
+    'libexo',
+    'libxfce4util',
+    'libxfce4menu',
+    'libxfcegui4',
+    'xfce-utils',
+    'xfconf',
+    'thunar',
+    'xfce4-dev-tools',
+    'xfce4-panel',
+    'xfce4-session',
+    'xfce4-settings',
+    'xfdesktop,xarchiver',
+    'xfce4-mixer,orage',
+    'xfwm4,xfwm4-themes',
+    'xfce4-appfinder',
+    'xfprint',
+    'libxfce4ui',
+    'xfburn',
+    'xfce4-places-plugin',
+    'xfce4-battery-plugin',
+    'xfce4-cddrive-plugin',
+    'thunar-volman',
+    'xfce4-power-manager',
+    'gigolo',
+    'parole'
+]
+
+def load_pkglist():
+    pass
+
+def wipe_out(dir):
+    dir = os.path.expanduser(dir)
+    print 'delete ',dir
+    sp.check_call(['rm','-rf',dir])
+    
+def chdir(dir):
+    dir = os.path.expanduser(dir)
+    print 'change directory:',dir
+    os.chdir(dir)
+
+def needsupdate_git(project, gitrepo):
+    wipe_out('~/git/' + project)
+    curgit = cpk.request_query(project, ilp)[0][1].trailingRevision().getVersion()
+    #curgit.getSourceVersion()
+    print 'latest version of ' + project + ' in conary is: ' + curgit
+    chdir('~/git/')
+    sp.check_call(['git-clone', '-q', '--depth', '1', '-n', gitrepo, project])
+    chdir('~/git/' + project)        
+    newgit =    sp.Popen(['git', 'rev-parse', '--short', 'HEAD'],
+            stdout=sp.PIPE, stderr=sp.STDOUT).stdout.read().strip()
+    print 'latest commit of ' + project + ' in git is: ' + newgit
+    chdir(cwd)
+    wipe_out('~/git/' + project)
+    return (newgit != curgit)
+
+def needsbuild(project):
+    srev = cpk.request_query(project +':source', ilp)[0][1].trailingRevision()
+    brev = cpk.request_query(project, ilp)[0][1].trailingRevision()
+    print srev,srev.buildCount,srev.sourceCount, brev,brev.buildCount,brev.sourceCount
+    return (srev.getVersion() != brev.getVersion() or
+            srev.sourceCount != brev.sourceCount)
+
+def refresh(project):
+    print 'refresh: ' + project
+    wipe_out(codir + project)
+    chdir(codir)
+    cvc.main(['refresh-xfce.py','co',project + '=' + ilp])
+    chdir(codir + project)
+    cvc.main(['refresh-xfce.py','refresh'])
+    cvc.main(['refresh-xfce.py','ci', "-m'sync with upstream'"])
+    chdir(cwd)
+    
+def update():
+    if needsupdate_git(project, gitrepo):
+        refresh(project)
+    
+    
 def build():
-	print ('build')
-	
+    if needsbuild(project):
+        print ('build')
+    rmk.main(['refresh-xfce.py', 'build', project + '{x86}', '--commit', '--context=fl:2-devel'])
+    
 parser = OptionParser(usage ="usage: %prog [options] action\n"
                       "\nActions:\n"
-					  "  update\t\tUpdate source\n"
-					  "  build\t\t\tbuild source",
-					  version=versionString)
-parser.add_option("-s", "--scm",
-                  help="Source Code Management: git, svn or all"
-				  " [default: %default]", default="all")
+                      "  update\t\tUpdate source\n"
+                      "  build\t\t\tbuild source",
+                      version=versionString)
+parser.add_option("-r", "--repo",
+                  help='Conary Repository:'
+                       'xd - xfce.rpath.org@xfce:devel '
+                       'fl2 - foresight.rpath.org@fl:2-devel '
+                       '[default: %default]', default="xd")
 
 (options, args) = parser.parse_args()
 print (options)
 print (args)
 if len(args) !=1:
-	parser.error("incorrect number of arguments")
+    parser.error("incorrect number of arguments")
 
+ilp = ilps[options.repo]
+codir = codirs[options.repo]
+project = 'whaawmp'
 if (args[0] == 'update'):
-	project = 'whaawmp'
-	gitrepo = 'http://git.gitorious.org/whaawmp/mainline.git'
-	update(options.scm)
+    gitrepo = 'http://git.gitorious.org/whaawmp/mainline.git'
+    update()
 elif (args[0] == 'build'):
-	build()
+    build()
 elif (args[0] == 'all'):
-	update(options.scm)
-	build()
+    update()
+    build()
 else:
-	parser.error("argument not allowed")
+    parser.error("argument not allowed")
